df7789b36c8654b56f502cc78e7d1ae731f714aa
Leandre <leajapo@gmail.com>
2022-01-09 22:07:38 +0200

    feat: added signin service function

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,21 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { BcryptService } from './utils/bcrypt.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository]), BcryptService],
-  providers: [AuthService],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_KEY,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, BcryptService, JwtStrategy],
   controllers: [AuthController],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 import { UserRepository } from './user.repository';
+
+import { BcryptService } from './utils/bcrypt.service';
+const bcryptService = new BcryptService();
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private readonly jwtService: JwtService,
   ) {}
 
   async signUp(authCredentials: AuthCredentialsDto): Promise<void> {
     return await this.userRepository.createUser(authCredentials);
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ token: string }> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.userRepository.findOne({ username });
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isValidPassword = await bcryptService.comparePassword(
+      password,
+      user.password,
+    );
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload: JwtPayload = { username: user.username, id: user.id };
+    const token = this.jwtService.sign(payload);
+
+    return { token };
   }
 }
